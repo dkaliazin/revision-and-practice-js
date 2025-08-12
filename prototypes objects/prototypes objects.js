12cd7afc8044db47dc8199ead1499a56bcc77595
dkaliazin <dkaliazin@gmail.com>
2025-08-12 21:16:00 +0300

    first task

--- prototypes objects/prototypes objects.js
+++ prototypes objects/prototypes objects.js
@@ -62,5 +62,37 @@ const tel_2 = {
   brand: "SAMSUNG",
 }
 
-tel.showInfo('China');
-tel.showInfo.call(tel_2,'South Korea')
+// tel.showInfo('China');
+// tel.showInfo.call(tel_2, 'South Korea')
+
+// 1
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
