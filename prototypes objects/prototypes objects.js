c97510852bbbc0590a6dd142ea361e33bf33aeb7
dkaliazin <dkaliazin@gmail.com>
2025-08-15 15:23:18 +0300

    simple class methods

--- prototypes objects/prototypes objects.js
+++ prototypes objects/prototypes objects.js
@@ -98,7 +98,7 @@ const customer = {
 // console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
 // 2
 
-// Створи клас Storage, який створюватиме об'єкти для управління складом товарів. 
+// Створи клас Storage, який створюватиме об'єкти для управління складом товарів.
 // Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного об'єкта в приватну властивість items.
 // Оголоси наступні методи класу:
 // getItems() — повертає масив поточних товарів у приватній властивості items.
@@ -106,6 +106,21 @@ const customer = {
 // removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
 // Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для
 //  перевірки коректності роботи.У консоль будуть виведені результати їх роботи.Будь ласка, нічого там не змінюй.
+class Storage{
+  #items;
+  constructor(items) {
+    this.#items= items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove)
+  }
+}
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
@@ -117,3 +132,40 @@ console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 storage.removeItem("Scaner");
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+// 3
+// Напиши клас StringBuilder, який приймає один параметр initialValue — 
+// довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
+// Оголоси наступні методи класу:
+// getValue() — повертає поточне значення приватної властивості value.
+// padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+// padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
+// padBoth(str) — отримує параметр str(рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, 
+// який викликає цей метод.
+// Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. 
+// У консоль будуть виведені результати їх роботи.Будь ласка, нічого там не змінюй.
+class StringBuilder{
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
